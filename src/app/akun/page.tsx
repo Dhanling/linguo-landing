a77808c47db362f4a0ab612147875329df62fcef
Dhanling <ramadhanimuhamadlutfi@gmail.com>
2026-04-19 14:51:39 +0700

    fix(akun): remove duplicate isTestPrep/enrollDays declarations

--- src/app/akun/page.tsx
+++ src/app/akun/page.tsx
@@ -533,9 +533,6 @@ function EnrollWizard({ showEnroll, setShowEnroll, enrollStep, setEnrollStep, en
   const isTestPrep = enrollProgram === "English Test Preparation";
   const enrollDays = Object.keys(enrollSchedule);
 
-  const isTestPrep = enrollProgram === "English Test Preparation";
-  const enrollDays = Object.keys(enrollSchedule);
-
   const DAYS = ["Senin","Selasa","Rabu","Kamis","Jumat","Sabtu","Minggu"];
   const TIMES = ["07:00","08:00","09:00","10:00","11:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00"];
 
